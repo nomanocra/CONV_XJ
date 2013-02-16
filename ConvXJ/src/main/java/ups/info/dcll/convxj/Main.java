ebcd15420d9762f285f2ac18a31a7756d9f10d17
Mehdii <Mehdii@Mehdii-PC.home>
2013-02-17 00:35:07 +0100

    delete unused variable

--- ConvXJ/src/main/java/ups/info/dcll/convxj/Main.java
+++ ConvXJ/src/main/java/ups/info/dcll/convxj/Main.java
@@ -1,10 +1,5 @@
 package ups.info.dcll.convxj;
 
-import java.io.InputStream;
-
-import net.sf.json.JSON;
-import net.sf.json.xml.XMLSerializer;
-import org.apache.commons.io.IOUtils;
 import ups.info.dcll.convxj.xmltojson.XmlToJson;
 
 /**
@@ -17,17 +12,4 @@ public class Main {
         XmlToJson xTj=new XmlToJson();
         xTj.convert();
     }
-    private static final String xmlFile = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-            + "<important-data certified=\"true\" processed=\"true\">\n"
-            + "  <timestamp>232423423423</timestamp>\n"
-            + "  <authors>\n"
-            + "    <author>\n"
-            + "      <firstName>Tim</firstName>\n"
-            + "      <lastName>Leary</lastName>\n"
-            + "    </author>\n"
-            + "  </authors>\n"
-            + "  <title>Flashbacks</title>\n"
-            + "  <shippingWeight>1.4 pounds</shippingWeight>\n"
-            + "  <isbn>978-0874778700</isbn>\n"
-            + "</important-data>";
 }
