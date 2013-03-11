83bc130da136bcb60f39c023703cd5a7d62b6752
CorNoX <alexis.paoleschi@gmail.com>
2013-03-11 16:38:51 +0100

    Modification du main qui affiche directement le GUI

--- ConvXJ/src/main/java/ups/info/dcll/convxj/Main.java
+++ ConvXJ/src/main/java/ups/info/dcll/convxj/Main.java
@@ -1,7 +1,8 @@
 package ups.info.dcll.convxj;
 
-import ups.info.dcll.convxj.xmltojson.XmlToJson;
-import ups.info.dcll.convxj.jsontoxml.JsonToXml;
+import java.io.FileNotFoundException;
+
+import ups.info.dcll.convxj.graphicinterface.GUI;
 /**
  * 
  * @author Racim Fahssi
@@ -10,21 +11,8 @@ import ups.info.dcll.convxj.jsontoxml.JsonToXml;
  */
 public class Main {
 
-    public static void main(String[] args) {
-        XmlToJson xTj = new XmlToJson();
-        xTj.convert();
-
-        String url = "moodle.json";
-        String newurl = Saver.makePath(url, "xml");
-        String textJSON = xTj.convert();
-
-        JsonToXml jTx = new JsonToXml(textJSON);
-
-        String xml = jTx.convert();
-
-        System.out.println(textJSON);
-        System.out.println(xml);
-
-        Saver.save(newurl, xml);
+    public static void main(String[] args) throws FileNotFoundException {
+		GUI gui = new GUI();
+		gui.setVisible(true);
     }
 }
