7ca7fa946f06a3576b821758e528ed4326ffaaf7
nomancra <dprt.david@hotmail.fr>
2013-03-12 14:16:45 +0100

    #1 création de la classe Test du GUI

---
+++ ConvXJ/src/test/java/ups/info/dcll/convxj/graphicinterface/TestGUI.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ups.info.dcll.convxj.graphicinterface;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author David
+ */
+public class TestGUI extends TestCase{
+    
+}
